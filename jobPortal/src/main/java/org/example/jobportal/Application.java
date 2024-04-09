26535bf78d003cc95d342e457cf2e817eae25a8e
Christoph Leitgeb <christophleitgeb2007@gmail.com>
2024-04-09 23:11:21 +0200

    added stylesheet

--- jobPortal/src/main/java/org/example/jobportal/Application.java
+++ jobPortal/src/main/java/org/example/jobportal/Application.java
@@ -5,12 +5,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 960, 540);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("application.css")).toExternalForm());
         stage.setTitle("Job Portal");
         stage.setScene(scene);
         stage.show();
